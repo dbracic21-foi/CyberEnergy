c8de51b93b1da19b9c93975a1f4ff648e08de9a6
dbracic21-foi <dbracic21@student.foi.hr>
2023-05-26 22:56:31 +0200

    Stvaranje forme za prijavu

---
+++ CyberEnergy/CyberEnergy/Program.cs
@@ -0,0 +1,24 @@
+﻿using DBLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CyberEnergy
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            DB.SetConfiguration("IPS23_dbracic21", "dbracic21", "oOe!?=TA");
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new frmPrijava());
+        }
+    }
+}
