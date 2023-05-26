4a6cb1e7e293dd61d254df17bc0aa36fc9e6b427
dbracic21-foi <dbracic21@student.foi.hr>
2023-05-26 22:59:18 +0200

    Stvaranje klase Korisnik i stvaranje apstrakne klase Persons

---
+++ CyberEnergy/CyberEnergy/Models/Korisnik.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberEnergy.Models
+{
+    public class Korisnik:Persons
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public bool CheckPassword(string password)
+        {
+            return Password == password;
+        }
+    }
+}
