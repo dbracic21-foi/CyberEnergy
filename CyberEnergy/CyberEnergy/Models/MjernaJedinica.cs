1bea4511cdc7d62ec985a4df351f798b848400cd
dbracic21-foi <dbracic21@student.foi.hr>
2023-05-27 10:01:32 +0200

    Stvaranje i implementacija klase VrstaEnergije i MjernaJedinica

---
+++ CyberEnergy/CyberEnergy/Models/MjernaJedinica.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberEnergy.Models
+{
+    public class MjernaJedinica
+    {
+        public int Id_VrstePotrosnje { get; set; }
+        public string NazivPotrosnje { get; set; }
+
+        public override string ToString()
+        {
+            return NazivPotrosnje;
+        }
+    }
+}
---
+++ CyberEnergy/CyberEnergy/Models/VrstaEnergije.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CyberEnergy.Models
+{
+    public class VrstaEnergije
+    {
+      public int Id_VrstePotrosnje { get; set; }
+        public string Naziv_Vrste_Potrosnje { get; set; }
+
+        public override string ToString()
+        {
+            return Naziv_Vrste_Potrosnje;
+        }
+
+    }
+}
